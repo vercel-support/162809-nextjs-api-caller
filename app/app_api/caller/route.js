2f61f0f6a7c3dd3bb4711c3b09449d39d1fc13b9
William Li <email@williamli.dev>
2023-11-03 22:05:20 +1100

    calling the new apis

--- app/app_api/caller/route.js
+++ app/app_api/caller/route.js
@@ -2,7 +2,7 @@ export async function GET() {
   console.log('app router: fetching time from another API...');
 
   const response = await fetch(
-    `https://162809-nextjs-api.preview.vercel-support.app/api/time`
+    `https://162809-nextjs-api.preview.vercel-support.app/api/time_for_app`
   );
   const data = await response.json();
 
--- pages/api/caller.js
+++ pages/api/caller.js
@@ -2,7 +2,7 @@ export default async (req, res) => {
   console.log('page router: fetching time from another API...');
 
   const response = await fetch(
-    `https://162809-nextjs-api.preview.vercel-support.app/api/time`
+    `https://162809-nextjs-api.preview.vercel-support.app/api/time_for_page`
   );
   const data = await response.json();
 
